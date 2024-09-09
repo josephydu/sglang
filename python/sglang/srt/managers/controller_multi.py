6026aa80b6c047472531fa0633985e4231e3ace3
YouYangxiu <yyx1824665362@gmail.com>
2024-09-09 13:27:37 +0800

    tmp

--- python/sglang/srt/managers/controller_multi.py
+++ python/sglang/srt/managers/controller_multi.py
@@ -176,13 +176,7 @@ class ControllerMulti:
         for i in range(len(tree_cache_list)):
             for j in range(len(input_requests)):
                 r = input_requests[j]
-                prefix_indices, last_node = tree_cache_list[i].match_prefix(
-                    rid=r.rid.clone(), key=r.adjust_max_prefix_ids().clone()
-                )
-
-                print(
-                    f"tree_cache={i}\t\trequest={j}\t\tprefix_indices={prefix_indices}\t\tprefix_indices_len={len(prefix_indices)}\t\tlast_nod{last_node}"
-                )
+                print(tree_cache_list[i])
         self.round_robin_scheduler(input_requests=input_requests)
         pass
 
