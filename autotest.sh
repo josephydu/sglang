28b360757990c4a74701619600c97ddb24ff2f3a
YouYangxiu <yyx1824665362@gmail.com>
2024-10-07 19:00:11 +0800

    dp vs tp

--- autotest.sh
+++ autotest.sh
@@ -12,7 +12,7 @@ echo "====================== $(date) ======================" >> "$LOG_FILE"
 
 # # 启动服务并将其放到后台，重定向输出到日志文件
 # echo "Running with setting: dp=4 tp=1 ======================================================" >> "$LOG_FILE"
-# /home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+# /home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
 #     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
 #     --dp-size 4 --load-balance-method resources_aware \
 #     --chunked-prefill-size 2048 --disable-radix-cache >> "$LOG_FILE" 2>&1 &
@@ -25,7 +25,7 @@ echo "====================== $(date) ======================" >> "$LOG_FILE"
 # # 循环从 7 到 12，步进为 0.5
 # # for rate in $(seq 3.5 0.25 6); do
 # # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-# /home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.bench_serving --backend sglang \
+# /home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.bench_serving --backend sglang \
 #         --host 127.0.0.1 --port 8080 --dataset-name random \
 #         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
 #         --random-output-len 1024 --random-input-len 4096 \
@@ -41,14 +41,14 @@ echo "====================== $(date) ======================" >> "$LOG_FILE"
 
 # 启动服务并将其放到后台，重定向输出到日志文件
 echo "Running with setting: dp=1 tp=8 ======================================================" >> "$LOG_FILE"
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 1 --tp-size 8 --load-balance-method resources_aware
 
 sleep 200
 # for rate in $(seq 3.5 0.25 6); do
     # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
         --random-output-len 1024 --random-input-len 4096 \
@@ -65,13 +65,13 @@ sleep 10
 # 启动服务并将其放到后台，重定向输出到日志文件
 echo "Running with setting: dp=2 tp=4 ======================================================" >> "$LOG_FILE"
 
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 2 --tp-size 4 --load-balance-method resources_aware >> "$LOG_FILE" 2>&1 &
 sleep 200
 # for rate in $(seq 3.5 0.25 6); do
     # echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
         --random-output-len 1024 --random-input-len 4096 \
@@ -89,11 +89,11 @@ sleep 10
 
 echo "Running with setting: dp=4 tp=2 ======================================================" >> "$LOG_FILE"
 
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 4 --tp-size 2 --load-balance-method resources_aware >> "$LOG_FILE" 2>&1 &
 sleep 200
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
         --random-output-len 1024 --random-input-len 4096 \
@@ -107,11 +107,11 @@ sleep 10
 
 echo "Running with setting: dp=8 tp=1 ======================================================" >> "$LOG_FILE"
 
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.launch_server --model-path Qwen/Qwen1.5-14B \
     --host 0.0.0.0 --port 8080 --mem-fraction-static 0.8 \
     --dp-size 8 --tp-size 1 --load-balance-method resources_aware >> "$LOG_FILE" 2>&1 &
 sleep 200
-/home/qspace/workspace/josephyou/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
+/home/qspace/workspace/josephyou/tools/bin/micromamba run -n sglang2 python3 -m sglang.iter_search_best_qps --backend sglang \
         --host 127.0.0.1 --port 8080 --dataset-name random \
         --tokenizer Qwen/Qwen1.5-14B --model Qwen/Qwen1.5-14B \
         --random-output-len 1024 --random-input-len 4096 \
