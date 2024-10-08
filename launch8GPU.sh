a1b82b2562d3244ac8fa0259814ec00b202eb202
YouYangxiu <yyx1824665362@gmail.com>
2024-10-08 19:15:57 +0800

    test register

--- launch8GPU.sh
+++ launch8GPU.sh
@@ -29,7 +29,7 @@ for (( gpu_index=0; gpu_index<n; gpu_index++ )); do
     
     # 启动服务
     echo "Starting service on GPU $gpu_index with port $port"
-    CUDA_VISIBLE_DEVICES=$gpu_index python3 -m sglang.launch_server --model-path /root/.cache/huggingface/hub/models--Qwen--Qwen2-7B/snapshots/453ed1575b739b5b03ce3758b23befdb0967f40e --host 0.0.0.0 --mem-fraction-static 0.7 --port $port &
+    CUDA_VISIBLE_DEVICES=$gpu_index python3 -m sglang.launch_server --model-path /root/.cache/huggingface/hub/models--Qwen--Qwen2-7B/snapshots/453ed1575b739b5b03ce3758b23befdb0967f40e --host 127.0.0.1 --mem-fraction-static 0.7 --port $port &
     
     # 保存进程 ID
     pids+=($!)
