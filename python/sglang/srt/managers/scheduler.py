cffdcfbb4131aa0d28accc9c2605d59e1be3727a
YouYangxiu <1824665362@qq.com>
2024-10-21 10:58:38 +0800

    add zmq radix

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -268,7 +268,7 @@ class Scheduler:
     def loop_for_send_tree_cache(self):
         while True:
             self.send_tree_cache_to_queue()
-            time.sleep(0.2)
+            time.sleep(1)
             
     def send_tree_cache_to_queue(self):
         if self.pre_radix:
