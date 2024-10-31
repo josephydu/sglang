64d9329dc284dce5f88fb9020fa80838e80bd092
YouYangxiu <yyx1824665362@gmail.com>
2024-10-31 11:34:37 +0800

    add time

--- benchmark/multi_turn_chat/data_gen.py
+++ benchmark/multi_turn_chat/data_gen.py
@@ -12,18 +12,36 @@ def gen_prompt(tokenizer, token_num):
     return ret
 
 
+import json
+
+
 def gen_arguments(args, tokenizer):
+    try:
+        with open(
+            f"generated_qas_{args.min_len_q}_{args.max_len_q}_{args.min_len_a}_{args.max_len_a}_{args.turns}_{args.num_qa}.json",
+            "r",
+        ) as json_file:
+            return json.load(json_file)
+    except Exception as e:
+        pass
     multi_qas = [{"qas": []} for _ in range(args.num_qa)]
     for i in range(args.num_qa):
         qas = multi_qas[i]["qas"]
-        for _ in range(args.turns):
+        turn_range = random.randint(1, args.turns)
+        for _ in range(turn_range):
             prompt_len = random.randint(args.min_len_q, args.max_len_q)
             new_tokens = random.randint(args.min_len_a, args.max_len_a)
             qas.append(
                 {
                     "prompt": gen_prompt(tokenizer, prompt_len),
                     "new_tokens": new_tokens,
+                    "id": i,
                 }
             )
 
+    with open(
+        f"generated_qas_{args.min_len_q}_{args.max_len_q}_{args.min_len_a}_{args.max_len_a}_{args.turns}_{args.num_qa}.json",
+        "w",
+    ) as json_file:
+        json.dump(multi_qas, json_file, indent=4)
     return multi_qas
