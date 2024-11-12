e1dd98661a68d960148023c40af0ed4335cdc2d2
Youyangxiu <yyx1824665362@gmail.com>
2024-11-12 20:58:40 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -375,8 +375,9 @@ class DataParallelController:
         # e.g. If the match nums is [1, 2, 0, 0, 0, 0], we think the scheduer method should be resources aware
         if max(prefix_lens) <= 100:
             # self.resources_aware_scheduler(req)
+            occipuied_lens = [(req_len - prefix_len) for req_len, prefix_len in zip(req_lens, prefix_lens)]
             gpu_idx = random.randint(0, self.dp_size)
-            self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - req_lens[gpu_idx]
+            self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
             self.workers[gpu_idx].send_pyobj(req)
         else:
             occipuied_lens = [(req_len - prefix_len) for req_len, prefix_len in zip(req_lens, prefix_lens)]
@@ -388,7 +389,7 @@ class DataParallelController:
             logger.info(f'[forward_mems]{forward_mems}')
             gpu_idx = forward_mems.index(max(forward_mems))
             logger.info(f'[gpu_idx]{gpu_idx}')
-            self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - req_lens[gpu_idx]
+            self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
 
         # gpu_idx = prefix_lens.index(max(prefix_lens))
         # self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
