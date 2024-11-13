d6901e4a1015a35da2323d2b1485e72a8173f99c
Youyangxiu <yyx1824665362@gmail.com>
2024-11-13 21:35:16 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -420,12 +420,16 @@ class DataParallelController:
                 logger.info(f'[main_available_kv_cache]{self.main_available_kv_cache}')
                 forward_mems = [(availiable - occipuied) if no_wait == 1 else (-100000) for availiable, occipuied, no_wait in zip(self.main_available_kv_cache, occipuied_lens, no_waiting)]
                 logger.info(f'[forward_mems]{forward_mems}')
-                gpu_idx = forward_mems.index(max(forward_mems))
-                logger.info(f'[request_id]{sum(req.input_ids[:1000])} go to [gpu_idx]{gpu_idx}')
-                # logger.info(f'[before minus]{self.main_available_kv_cache}')
-                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
-                # self.main_evictable_kv_cache[gpu_idx] = self.main_evictable_kv_cache[gpu_idx] + occipuied_lens[gpu_idx]
-                # logger.info(f'[after minus]{self.main_available_kv_cache}\n')
+
+                if max(forward_mems) < 0:
+                    gpu_idx = self.allocate_gpu(req)
+                else:
+                    gpu_idx = forward_mems.index(max(forward_mems))
+                    # logger.info(f'[before minus]{self.main_available_kv_cache}')
+                    self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
+                    # self.main_evictable_kv_cache[gpu_idx] = self.main_evictable_kv_cache[gpu_idx] + occipuied_lens[gpu_idx]
+                    # logger.info(f'[after minus]{self.main_available_kv_cache}\n')
+                logger.info(f'[request_id]{sum(req.input_ids[:1000])} go to [gpu_idx]{gpu_idx}\n')
                 self.workers[gpu_idx].send_pyobj(req)
 
     def shortest_queue_scheduler(self, input_requests):
