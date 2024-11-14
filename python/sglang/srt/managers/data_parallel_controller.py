a0334b327b86c509d667f5c05b2ec21f1d78864f
Youyangxiu <yyx1824665362@gmail.com>
2024-11-14 20:01:36 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -373,7 +373,11 @@ class DataParallelController:
             forward_mems = [(availiable - occipuied - evictbale) if no_wait == 1 else (-100000) for availiable, occipuied, no_wait, evictbale in zip(self.main_available_kv_cache, occipuied_lens, no_waiting, self.main_evictable_kv_cache)]
             logger.info(f'[forward mems]{forward_mems}')
             if max(forward_mems) < 0:
-                gpu_idx = prefix_lens.index(max(prefix_lens))
+                max_prefix = max(prefix_lens)
+                max_indices = [
+                    index for index, value in enumerate(max_prefix) if value == max_prefix
+                ]
+                gpu_idx = random.choice(max_indices)
             else:
                 gpu_idx = forward_mems.index(max(forward_mems))
             logger.info(f'[before minus2]{self.main_available_kv_cache}')
