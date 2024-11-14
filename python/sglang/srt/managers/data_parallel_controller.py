794a19c79642f1875b511b5bb98770955da95e4e
Youyangxiu <yyx1824665362@gmail.com>
2024-11-14 11:24:08 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -431,6 +431,7 @@ class DataParallelController:
             if all_waiting:
                 gpu_idx = self.allocate_gpu(req)
                 # logger.info(f'[resources_aware_scheduler][request_id]{sum(req.input_ids[:1000])} go to [gpu_idx]{gpu_idx}')
+                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - len(req.input_ids)
                 self.workers[gpu_idx].send_pyobj(req)
             else:
                 # select no waiting queue, if waitting, the available is meaningless, we set it to zero.
@@ -440,8 +441,8 @@ class DataParallelController:
                 # logger.info(f'[prefix_lens]{prefix_lens}')
                 # logger.info(f'[occipuied_lens]{occipuied_lens}')
                 # logger.info(f'[main_available_kv_cache]{self.main_available_kv_cache}')
-                # forward_mems = [(availiable - occipuied - evitable) if no_wait == 1 else (-100000) for availiable, occipuied, no_wait, evitable in zip(self.main_available_kv_cache, occipuied_lens, no_waiting, self.main_evictable_kv_cache)]
-                forward_mems = [(availiable - occipuied - evitable) for availiable, occipuied, evitable in zip(self.main_available_kv_cache, occipuied_lens, self.main_evictable_kv_cache)]
+                forward_mems = [(availiable - occipuied - evitable) if no_wait == 1 else (-100000) for availiable, occipuied, no_wait, evitable in zip(self.main_available_kv_cache, occipuied_lens, no_waiting, self.main_evictable_kv_cache)]
+                # forward_mems = [(availiable - occipuied - evitable) for availiable, occipuied, evitable in zip(self.main_available_kv_cache, occipuied_lens, self.main_evictable_kv_cache)]
                 # logger.info(f'[forward_mems]{forward_mems}')
 
                 if max(forward_mems) < 0:
@@ -449,7 +450,7 @@ class DataParallelController:
                 else:
                     gpu_idx = forward_mems.index(max(forward_mems))
                 # logger.info(f'before{self.main_available_kv_cache[gpu_idx]}')
-                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
+                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx] - req.sampling_params.max_new_tokens * 0.5
                 # logger.info(f'after{self.main_available_kv_cache[gpu_idx]}')
                 # logger.info(f'[request_id]{sum(req.input_ids[:1000])} go to [gpu_idx]{gpu_idx}\n')
                 self.workers[gpu_idx].send_pyobj(req)
