cbf5d4f9544c0eaa637fcd03a3a788de4114a963
Youyangxiu <yyx1824665362@gmail.com>
2024-11-14 13:17:24 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -367,7 +367,7 @@ class DataParallelController:
             else:
                 # select no waiting queue, if waitting, the available is meaningless, we set it to zero.
                 # find target max
-                occipuied_lens = [(req_len - prefix_len + req.sampling_params.max_new_tokens * 0.5) for req_len, prefix_len in zip(req_lens, prefix_lens)]
+                occipuied_lens = [(req_len - prefix_len) for req_len, prefix_len in zip(req_lens, prefix_lens)]
                 # logger.info(f'[req_lens]{req_lens}')
                 # logger.info(f'[prefix_lens]{prefix_lens}')
                 # logger.info(f'[occipuied_lens]{occipuied_lens}')
@@ -380,7 +380,7 @@ class DataParallelController:
                 else:
                     gpu_idx = forward_mems.index(max(forward_mems))
                 # logger.info(f'before{self.main_available_kv_cache[gpu_idx]}')
-                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
+                self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx] - req.sampling_params.max_new_tokens * 0.5
                 # logger.info(f'after{self.main_available_kv_cache[gpu_idx]}')
                 # logger.info(f'[request_id]{sum(req.input_ids[:1000])} go to [gpu_idx]{gpu_idx}\n')
                 self.workers[gpu_idx].send_pyobj(req)
