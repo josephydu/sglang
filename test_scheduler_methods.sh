a931f469c51b2e7d407eb88b27cb438fbf088a61
Youyangxiu <yyx1824665362@gmail.com>
2024-11-18 19:27:45 +0800

    improve pre_radix

--- test_scheduler_methods.sh
+++ test_scheduler_methods.sh
@@ -16,11 +16,8 @@ declare -A settings
 settings["resources_aware"]="dp8 pre_radix"
 settings["round_robin"]="dp8 round_robin"
 
-for random_output_len in 128 256 512; do
-    for random_input_len in 512 1024 2048; do
-        for rate in 7.0 7.1 7.2 7.3 7.4 7.5 7.6 7.7 7.8 7.9; do
-        # for rate in 9.0 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9; do
-        # for rate in 8.0 8.1 8.2 8.3 8.4 8.5 8.6 8.7 8.8 8.9; do
+for random_output_len in 128 256 512 1024 2048 4096; do
+    for random_input_len in 128 256 512 1024 2048 4096; do
             # 循环处理每个设置
             for method in "${!settings[@]}"; do
                 setting=${settings[$method]}
@@ -39,11 +36,10 @@ for random_output_len in 128 256 512; do
                 # for rate in $(seq 16 0.1 16.2); do
                 # 启动 benchmark 脚本，重定向输出到日志文件
                 # 循环从 16 到 20，步进为 0.1
-                echo "Running with request-rate: $rate" | tee -a "$LOG_FILE"  # 输出当前的 request-rate 值并追加到日志文件
                 /workspace/bin/micromamba run -n sglang python benchmark/multi_turn_chat/long_prompt_multi_turn.py \
                 --tokenizer Llama-3.1-8B-Instruct --port 30000 \
                 --parallel 155 \
-                --len-q 32 --len-a 128 \
+                --len-q $random_input_len --len-a $random_output_len \
                 --turns 11 --num-qa 1024
 
                 sleep 100
@@ -51,9 +47,10 @@ for random_output_len in 128 256 512; do
 
                 # 杀死特定的 Python 进程
                 ps -elf | grep '[p]ython' | awk '{print $4}' | xargs kill -s 9
+                ps -elf | grep '[p]ython' | awk '{print $4}' | xargs kill -s 9
 
                 # 等待一段时间以确保进程被杀死
-                sleep 300
+                sleep 30
             done
         done
     done
