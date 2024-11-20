9b7b2e1a139629df22b86d1fc52cbfcad1bf678c
Youyangxiu <yyx1824665362@gmail.com>
2024-11-20 14:22:47 +0800

    improve pre_radix

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -385,11 +385,9 @@ class DataParallelController:
             #================method1
             min_run = min(self.main_num_running_req)
             threshold = min_run + 3
-
             min_run_indices = [idx for idx, value in enumerate(self.main_num_running_req) if value <= threshold]
             max_len = max(prefix_lens[idx] for idx in min_run_indices)
             gpus_candicate = [idx for idx in min_run_indices if prefix_lens[idx] == max_len]
-
             gpu_idx = random.choice(gpus_candicate)
             # =====================bad method1  400s+
             
@@ -421,9 +419,9 @@ class DataParallelController:
             # ]
             # gpu_idx = random.choice(max_indices)
             # #==================
-            # self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
-            # self.main_num_running_req[gpu_idx] += 1
-            # self.workers[gpu_idx].send_pyobj(req)
+            self.main_available_kv_cache[gpu_idx] = self.main_available_kv_cache[gpu_idx] - occipuied_lens[gpu_idx]
+            self.main_num_running_req[gpu_idx] += 1
+            self.workers[gpu_idx].send_pyobj(req)
 
     def shortest_queue_scheduler(self, input_requests):
         raise NotImplementedError()
