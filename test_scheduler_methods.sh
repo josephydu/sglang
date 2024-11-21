f421962804b64f2f9c2353544878ac75a7a53dd5
Youyangxiu <yyx1824665362@gmail.com>
2024-11-21 16:04:04 +0800

    improve pre_radix

--- test_scheduler_methods.sh
+++ test_scheduler_methods.sh
@@ -25,7 +25,7 @@ for random_output_len in 128 256 512 1024 2048 4096; do
                 
                 # 启动服务并将其放到后台，重定向输出到日志文件
                 /workspace/bin/micromamba run -n sglang python3 -m sglang.launch_server \
-                    --model-path Llama-3.1-8B/ \
+                    --model-path Llama-3.1-8B-Instruct \
                     --host 127.0.0.1 --port 8080 --mem-fraction-static 0.7 \
                     --dp-size 8 \
                     --load-balance-method $method >> "$LOG_FILE" 2>&1 &
