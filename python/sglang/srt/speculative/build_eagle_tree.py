a56f2b7c2824e82e7d206d4ad2b90ff59e964dcb
josephyou <josephyou@tencent.com>
2025-01-20 19:33:06 +0800

    info

--- python/sglang/srt/speculative/build_eagle_tree.py
+++ python/sglang/srt/speculative/build_eagle_tree.py
@@ -47,8 +47,8 @@ __global__ void build_tree(Tensor<long, 2> parent_list, Tensor<long, 2> selected
             }
 
             int token_idx = parent_list[bid][parent_tb_idx];
-            for(cur_position = 0; cur_position<draft_token_num ;cur_position++){
-                printf("check equal: left=%d, right=%d, equal=%d\\n", selected_index[bid][cur_position], token_idx,selected_index[bid][cur_position]==token_idx);
+            for(cur_position=0; cur_position<draft_token_num;cur_position++){
+                printf("check equal: left=%d, right=%d, equal=%d\\n", selected_index[bid][cur_position], token_idx,selected_index[bid][cur_position]==token_idx?1:0);
                 if(selected_index[bid][cur_position]==token_idx){
                     break;
                 }
