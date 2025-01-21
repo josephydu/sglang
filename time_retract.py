cd7a527ca39f918ad56fc8c1fd53d1b594ea613c
josephyou <josephyou@tencent.com>
2025-01-21 14:20:54 +0800

    info

---
+++ time_retract.py
@@ -0,0 +1,24 @@
+import re
+
+txt = """
+[build_tree_kernel time] = 0.09844017028808594
+[build_tree_kernel time] = 0.0008730888366699219
+[build_tree_kernel time] = 0.0007734298706054688
+[build_tree_kernel time] = 0.0007698535919189453
+[build_tree_kernel time] = 0.0007941722869873047
+[build_tree_kernel time] = 0.0007610321044921875
+[build_tree_kernel time] = 0.0007638931274414062
+[build_tree_kernel time] = 0.0007860660552978516
+[build_tree_kernel time] = 0.0007777214050292969
+[build_tree_kernel time] = 0.0007762908935546875
+[build_tree_kernel time] = 0.0007712841033935547
+[build_tree_kernel time] = 0.0009343624114990234
+[build_tree_kernel time] = 0.0008308887481689453
+[build_tree_kernel time] = 0.0008482933044433594
+[build_tree_kernel time] = 0.0008594989776611328
+[build_tree_kernel time] = 0.0008947849273681641
+[build_tree_kernel time] = 0.0009062290191650391
+[build_tree_kernel time] = 0.0008127689361572266
+[build_tree_kernel time] = 0.0007724761962890625
+[build_tree_kernel time] = 0.0007605552673339844
+"""
