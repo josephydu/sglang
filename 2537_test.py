8fb1196e76a54dbf2ddebd0b1545f82dff3b4fc8
josephyou <josephyou@tencent.com>
2025-01-21 10:28:01 +0800

    info

---
+++ 2537_test.py
@@ -0,0 +1,21 @@
+import openai
+
+client = openai.Client(base_url="http://127.0.0.1:30000/v1", api_key="None")
+
+for _ in range(10):
+    response = client.chat.completions.create(
+        model="/workspace/Qwen2-7B-Instruct/",
+        messages=[
+            {
+                "role": "user",
+                "content": 'What are the mental triggers in Jeff Walker\'s Product Launch Formula and "Launch" book?',
+            },
+        ],
+        temperature=0,
+        max_tokens=1024,
+    )
+    completion_tokens = response.usage.completion_tokens
+    completion_tokens_wo_jump_forward = response.usage.completion_tokens_wo_jump_forward
+    print(
+        f"{completion_tokens=}, sample_tokens = {completion_tokens/completion_tokens_wo_jump_forward}"
+    )
