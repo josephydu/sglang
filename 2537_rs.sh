f35dede3a4014f67ce224471d5c8629a2b260937
josephyou <josephyou@tencent.com>
2025-01-21 10:46:43 +0800

    info

--- 2537_rs.sh
+++ 2537_rs.sh
@@ -1,5 +1,5 @@
-python3 -m sglang.launch_server --model /workspace/Qwen2-7B-Instruct \
+python3 -m sglang.launch_server --model /workspace/Llama-2-7b-chat-hf \
 --max-prefill-tokens 16384  --trust-remote-code --tp 1 --dp 1  --mem-fraction-static 0.5 \
---speculative-draft /workspace/EAGLE-Qwen2-7B-Instruct \
+--speculative-draft /workspace/sglang-EAGLE-llama2-chat-7B \
 --speculative-num-steps 4 --speculative-eagle-topk 2 --speculative-num-draft-tokens 8 --speculative-algo EAGLE \
 --disable-radix-cache
