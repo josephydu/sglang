8ab184d7998ea2fe9b19add1d37c7c18de0d651d
josephyou <josephyou@tencent.com>
2025-02-07 15:35:20 +0800

    add

--- python/sglang/srt/models/wepoints.py
+++ python/sglang/srt/models/wepoints.py
@@ -574,6 +574,7 @@ class POINTSV15ChatModel(nn.Module):
             ("gate_up_proj", "gate_proj", 0),
         ]
         params_dict = dict(self.named_parameters(remove_duplicate=False))
+        print(params_dict)
         for name, loaded_weight in weights:
             if "rotary_emb.inv_freq" in name:
                 continue
