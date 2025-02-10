2e310d1ab1ac157b0a4cb7678c0e29197a5e59cb
josephyou <josephyou@tencent.com>
2025-02-10 14:57:09 +0800

    wepoints suit sglang

--- test_wepoints_transformer.py
+++ test_wepoints_transformer.py
@@ -41,9 +41,9 @@ image_processor = AutoProcessor.from_pretrained(model_path).image_processor
 # image_processor = Qwen2ImageProcessorForPOINTSV15.from_pretrained(model_path)
 
 image_url = "/ocr.png"
-response = requests.get(image_url)
-image_data = BytesIO(response.content)
-pil_image = Image.open(image_data)
+# response = requests.get(image_url)
+# image_data = BytesIO(response.content)
+pil_image = Image.open(image_url)
 pil_image = pil_image.save("image.jpg")
 prompt = "提取图片中所有的文字"
 
