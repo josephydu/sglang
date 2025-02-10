1d8891a01b763fb0624240260e98d10cd5bc1a22
josephyou <josephyou@tencent.com>
2025-02-10 13:35:23 +0800

    wepoints suit sglang

---
+++ test_wepoints_sglang.py
@@ -0,0 +1,28 @@
+import requests
+
+url = "http://localhost:30000/v1/chat/completions"
+
+data = {
+    "model": "WePoints",
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "please describe the image in detail"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://github.com/user-attachments/assets/83258e94-5d61-48ef-a87f-80dd9d895524"
+                    },
+                },
+            ],
+        }
+    ],
+    "max_new_tokens": 1024,
+    "temperature": 0.0,
+    "top_p": 0.0,
+    "num_beams": 1,
+}
+
+response = requests.post(url, json=data)
+response(response.text)
