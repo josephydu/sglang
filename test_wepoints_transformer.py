b321bdde5e38be50931527fc198121fad3a464af
josephyou <josephyou@tencent.com>
2025-02-10 11:11:12 +0800

    wepoints suit sglang

---
+++ test_wepoints_transformer.py
@@ -0,0 +1,53 @@
+from typing import Dict
+
+import requests
+import torch
+from PIL import Image
+from torchvision import io
+from transformers import (
+    AutoModelForCausalLM,
+    AutoProcessor,
+    AutoTokenizer,
+    Qwen2VLForConditionalGeneration,
+)
+
+# Load the model in half-precision on the available device(s)
+model = AutoModelForCausalLM.from_pretrained("/WePoints", device_map="auto")
+processor = AutoProcessor.from_pretrained("/WePoints")
+
+# Image
+url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+conversation = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+            },
+            {"type": "text", "text": "Describe this image."},
+        ],
+    }
+]
+
+
+# Preprocess the inputs
+text_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+# Excepted output: '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe this image.<|im_end|>\n<|im_start|>assistant\n'
+
+inputs = processor(
+    text=[text_prompt], images=[image], padding=True, return_tensors="pt"
+)
+inputs = inputs.to("cuda")
+
+# Inference: Generation of the output
+output_ids = model.chat(**inputs, max_new_tokens=128)
+generated_ids = [
+    output_ids[len(input_ids) :]
+    for input_ids, output_ids in zip(inputs.input_ids, output_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+)
+print(output_text)
