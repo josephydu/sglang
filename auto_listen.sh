cb43dfe6a006a9499057b495f8fc72dda29cff2a
josephyou <josephyou@tencent.com>
2025-02-18 19:59:02 +0800

    benchmark

--- auto_listen.sh
+++ auto_listen.sh
@@ -2,13 +2,16 @@
 
 # 定义要检查的 URL
 URL="http://127.0.0.1:30000/health"
+CHECK_INTERVAL=10  # 检查间隔
 
 # 先清除服务
+echo "正在清除其他服务..."
 ps -elf | grep sglang  | awk '{print $4}' | xargs  kill -s 9
 sleep 10  # 等待服务关闭
+echo "清除完毕..."
 
 # 设置启动命令
-MEM_FRACTION_STATIC="--mem-fraction-static 0.6"
+MEM_FRACTION_STATIC="0.6"
 MODEL_PATH="/WePoints/"
 DP="1"
 START_COMMAND="python3 -m sglang.launch_server --model-path $MODEL_PATH --trust-remote-code --chat-template qwen2-vl --mem-fraction-static $MEM_FRACTION_STATIC --dp $DP"
@@ -16,13 +19,15 @@ START_COMMAND="python3 -m sglang.launch_server --model-path $MODEL_PATH --trust-
 SERVER_LOG_FILE="server.log"
 # 尝试第一次启动服务
 echo "正在启动服务..."
-# 向文件中写入此时的时间
+echo "启动命令"
+echo "$START_COMMAND"
+# 向日志文件中写入此时的时间
 echo "==================启动时间: $(date)==================" >> "$SERVER_LOG_FILE"
 eval $START_COMMAND >> "$SERVER_LOG_FILE" 2>&1 &
 sleep 60  # 等待60s服务启动
+echo "启动成功，开始监听服务状态..."
 
 
-CHECK_INTERVAL=10  # 检查间隔
 while true; do
     # 使用 curl 检查服务健康状态
     response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
