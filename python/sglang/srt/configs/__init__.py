5ff7d8fc1058159d5d6c4a746c8e3997f1ef17a6
josephyou <josephyou@tencent.com>
2025-02-19 12:01:53 +0800

    add

--- python/sglang/srt/configs/__init__.py
+++ python/sglang/srt/configs/__init__.py
@@ -2,8 +2,9 @@ from sglang.srt.configs.chatglm import ChatGLMConfig
 from sglang.srt.configs.dbrx import DbrxConfig
 from sglang.srt.configs.exaone import ExaoneConfig
 from sglang.srt.configs.pointsv15_chat import POINTSV15ChatConfig
-from sglang.srt.configs.qwen2_5_vl_config import (  # Qwen2_5_VLVisionConfig,
+from sglang.srt.configs.qwen2_5_vl_config import (
     Qwen2_5_VLConfig,
+    Qwen2_5_VLVisionConfig,
 )
 
 __all__ = [
@@ -12,5 +13,5 @@ __all__ = [
     "DbrxConfig",
     "POINTSV15ChatConfig",
     "Qwen2_5_VLConfig",
-    # "Qwen2_5_VLVisionConfig",
+    "Qwen2_5_VLVisionConfig",
 ]
