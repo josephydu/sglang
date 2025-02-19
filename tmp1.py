ae5b83b00e8e1ba4ff4b6d24fe8d705a7fd870ba
josephyou <josephyou@tencent.com>
2025-02-19 11:08:27 +0800

    add

--- tmp1.py
+++ tmp1.py
@@ -11,35 +11,32 @@ test_pngs = [
     "test_pngs/test4.png",
     "test_pngs/test5.png",
 ]
-data = {
-    "model": "WePoints",
-    "messages": [
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "图片里面有什么?"},
-                {
-                    "type": "image_url",
-                    "image_url": {"url": "/tests_pngs/test2.png"},
-                },
-            ],
-        }
-    ],
-    "max_tokens": 4096,
-    "temperature": 0,
-}
 
-try:
-    while True:
-        # 发送 data1
-        response = requests.post(url, json=data1)
-        print("Response for test1.png:", response.text)
+
+def construct_data(png_path):
+    data = {
+        "model": "WePoints",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "图片里面有什么?"},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"{png_path}"},
+                    },
+                ],
+            }
+        ],
+        "max_tokens": 4096,
+        "temperature": 0,
+    }
+    return data
+
+
+for test_png in test_pngs:
+    for png in test_pngs:
+        data = construct_data(png)
+        response = requests.post(url, json=data)
+        print(f"Response for {png}:", response.text)
         time.sleep(0.5)  # 等待0.5秒
-
-        # 发送 data2
-        response = requests.post(url, json=data2)
-        print("Response for test2.png:", response.text)
-        time.sleep(0.5)  # 等待0.5秒
-
-except KeyboardInterrupt:
-    print("\nStopped by user")
