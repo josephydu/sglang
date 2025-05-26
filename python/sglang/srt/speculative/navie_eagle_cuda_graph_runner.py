f0e929649a36ae42c120c919940f5d2eaaaf6976
josephyou <josephyou@tencent.com>
2025-05-26 18:28:56 +0800

    1

--- python/sglang/srt/speculative/navie_eagle_cuda_graph_runner.py
+++ python/sglang/srt/speculative/navie_eagle_cuda_graph_runner.py
@@ -636,7 +636,7 @@ class NaiveEAGLECudaGraphRunner:
         # Replay
         self.graphs[self.bs].replay()
         next_token_logits, hidden_states, next_token_ids, accept_index, draft_logits_output, draft_input = self.output_buffers[self.bs]
-        logger.info(f'[again check]{self.bs=},{self.raw_bs=},{forward_batch.batch_size=},{draft_logits_output.hidden_satates.shape=}')
+        logger.info(f'[again check]{self.bs=},{self.raw_bs=},{forward_batch.batch_size=},{draft_logits_output.hidden_states.shape=}')
         
         next_token_ids = next_token_ids[: self.raw_num_token]
         accept_index = accept_index[: self.raw_bs]
