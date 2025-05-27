ac2ec1b19fd7781be886cac79f2a976baca8ff72
josephyou <josephyou@tencent.com>
2025-05-27 17:25:18 +0800

    1

--- test/srt/test_eagle_infer.py
+++ test/srt/test_eagle_infer.py
@@ -49,7 +49,7 @@ class TestEAGLEEngine(CustomTestCase):
         # "disable_cuda_graph": True, 
         "disable_overlap_schedule": True,
         "requests_all_greedy": False,
-        "tp-size": 2
+        "tp_size": 2
     }
     NUM_CONFIGS = 1
 
